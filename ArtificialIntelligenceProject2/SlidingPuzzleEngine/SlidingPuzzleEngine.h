2b53788440bfbe4a0bd39f6d9a85669ee4345530
이화중 <hj_tl22@live.co.kr>
2016-11-20 16:20:27 +0900

    Initial Library Project

---
+++ ArtificialIntelligenceProject2/SlidingPuzzleEngine/SlidingPuzzleEngine.h
@@ -0,0 +1,10 @@
+﻿// SlidingPuzzleEngine.h
+//
+
+#pragma once
+
+#ifdef SLIDINGPUZZLEENGINE_EXPORTS
+#define SLIDINGPUZZLE_API __declspec(dllexport)	// DLL로 함수를 내보냅니다.
+#else
+#define SLIDINGPUZZLE_API __declspec(dllimport)	// 응용 프로그램에서 함수의 가져오기를 최적화합니다.
+#endif
